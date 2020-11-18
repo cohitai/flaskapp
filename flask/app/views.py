3a9c7d167482a8bede24ae5716bc43b2b8d73910
cohitai <cohitai@gmail.com>
2020-11-18 18:55:02 +0100

    exclusion queries added

--- flask/app/views.py
+++ flask/app/views.py
@@ -12,6 +12,9 @@ from functools import wraps
 
 app.config['SECRET_KEY'] = 'thisisthesecretkey'
 
+path_to_model = 'prediction/model.pkl'
+path_to_exclusion = 'exclusion/ext.pkl'
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -20,7 +23,7 @@ def token_required(f):
         if not token:
             return jsonify({'message' : 'Token is missing!'}), 403
 
-        try: 
+        try:
             data = jwt.decode(token, app.config['SECRET_KEY'])
         except:
             return jsonify({'message' : 'Token is invalid!'}), 403
@@ -33,44 +36,75 @@ def token_required(f):
 def home():
     return render_template('index.html')
 
-
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # os.chdir(os.path.dirname(__file__))
-        # print("current workplace:", os.getcwd())
         f = request.files['file']
         global model
         model = pickle.load(f)
-        #contents = f.read()
-        pickle.dump(model, open('model.pkl', 'wb'))
-        #model.save("model1.pkl")
+        pickle.dump(model, open(path_to_model, 'wb'))
         return 'file uploaded successfully'
 
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    # global model 
-    model = pickle.load(open("model.pkl", 'rb'))
-    # print(model)
+    model = pickle.load(open(path_to_model, 'rb'))
     docid = next(request.form.values())
     prediction = model[int(docid)]
 
     return render_template('index.html', prediction_text1='{}'.format(prediction[0]), prediction_text2='{}'.format(prediction[1]), prediction_text3='{}'.format(prediction[2])
                            , prediction_text4='{}'.format(prediction[3]), prediction_text5='{}'.format(prediction[4]))
 
-
 @app.route('/query/<int:docid>', methods=['GET'])
 def get_prediction(docid):
+    model = pickle.load(open(path_to_model, 'rb'))
     while True: 
         try:
             return jsonify({"prediction": model[docid]})
         except NameError:
             time.sleep(.5)
 
+@app.route('/exclude/<int:docid>', methods=['GET'])
+def get_exclusion(docid):
+
+    if not os.path.exists(path_to_exclusion):
+        with open(path_to_exclusion, 'wb') as f:
+            pickle.dump([],f)
+
+    with open(path_to_exclusion, 'rb') as f:
+        list_ex = pickle.load(f)
+        list_ex.append(docid)
+    with open(path_to_exclusion,'wb') as f:
+        pickle.dump(list_ex,f)
+    try:
+        return 'DocId {0} is now excluded'.format(docid)
+    except NameError:
+        time.sleep(.5)
+
+@app.route('/include/<int:docid>', methods=['GET'])
+def reinclude(docid):
+
+    with open(path_to_exclusion, 'rb') as f:
+        exclusion_data = pickle.load(f)
+        try:
+            exclusion_data.remove(docid)
+        except ValueError:
+            pass
+    with open(path_to_exclusion, 'wb') as f:
+        pickle.dump(exclusion_data, f)
+    
+    return jsonify({"new excluded articles list:": str(exclusion_data)})
+
+@app.route('/exclusions_list', methods=['GET'])
+def list_ext():
+    
+    exclusion_data = []
+    with open(path_to_exclusion, 'rb') as f:
+        return jsonify({"excluded articles": str( pickle.load(f))})
 
 @app.route('/query_1', methods=['GET'])
 def relative():
+    model = pickle.load(open(path_to_model, 'rb'))
     while True:
         try: 
             return jsonify({"relative": str(sorted(list(model.keys())))})
@@ -86,7 +120,7 @@ def protected():
 def login():
     auth = request.authorization
 
-    if auth and auth.password == 'einberliner':
+    if auth.username == 'Livingdocs' and auth.password == 'einberliner':
         #token = jwt.encode({'iat': datetime.datetime.utcnow(), 'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'])
         token = jwt.encode({'iat': datetime.datetime.utcnow(), 'user' : auth.username, "scope": "public-api:read", "type": "client"}, app.config['SECRET_KEY'])
         return jsonify({'token' : token.decode('UTF-8')})
